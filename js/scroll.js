0dde9510bcd4acd850dcfaf5af267d1154c5e8f4
MaseJ <mjohnson434@student.cvtc.edu>
2022-05-22 23:22:02 -0500

    Format index and add js

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+// Create list of nav and add on click to them
+var links = document.querySelectorAll(".scroll-to");
+links.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        var elm = document.getElementById(item.getAttribute("data-link"));
+        elm.scrollIntoView({behavior:"smooth", block:"center"})
+    })
+})
+
+  
