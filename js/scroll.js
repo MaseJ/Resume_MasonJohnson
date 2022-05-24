20fb24f1e210c7e6dc42c50d4a28e328e9a3f9eb
MaseJ <mjohnson434@student.cvtc.edu>
2022-05-24 11:26:17 -0500

    Fix scroll animation

--- js/scroll.js
+++ js/scroll.js
@@ -3,8 +3,11 @@ var links = document.querySelectorAll(".scroll-to");
 links.forEach((item)=>{
     item.addEventListener("click", ()=>{
         var elm = document.getElementById(item.getAttribute("data-link"));
-        elm.scrollIntoView({behavior:"smooth", block:"center"})
-    })
-})
+        elm.scrollIntoView({
+            behavior: 'smooth',
+            block: "center"
+        });
+    });
+});
 
   
